f35d4bd50f7b755bbd26dffb2508b85aa4e5752c
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-27 02:25:57 +0300

    update timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,83 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+
+const inputTimer = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+
+startBtn.setAttribute('disabled',true)
+startBtn.addEventListener('click', onStartBtnClick);
+
+let intervalId = null;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        const currentTime = options.defaultDate
+        const deltaTime = selectedDates[0] - currentTime;
+        
+        if (deltaTime < 0) {
+            return alert("Please choose a date in the future")
+            
+        }  else {
+            startBtn.removeAttribute('disabled');
+            options.selectedDates = selectedDates[0].getTime();
+
+        };
+
+    //   console.log(selectedDates[0]);
+    },
+};
+
+
+inputTimer.addEventListener('input', flatpickr('input[type="text"]', options));
+
+function onStartBtnClick() {
+   const currentTime = Date.now();
+//    const selectedDates = options.selectedDates
+   
+
+    intervalId = setInterval(()=>{
+        
+        const selectedDates = options.selectedDates
+        const countdownTimer =  selectedDates  - currentTime;
+        const time = convertMs(countdownTimer)
+        console.log('time',time);
+       
+
+
+    }, 1000);
+
+    
+};
+
+
+
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+  
+const date = Date.now()
+console.log(date);
+
+ 
